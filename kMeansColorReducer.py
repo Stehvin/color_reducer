be8e9545172fde6789f6af5ed8d73732a824c4c8
Stehvin <stehvin.olson@gmail.com>
2017-06-05 20:37:58 -0500

    Add image read and centroid initialization function

--- kMeansColorReducer.py
+++ kMeansColorReducer.py
@@ -1,32 +1,50 @@
 # -*- coding: utf-8 -*-
 
+import scipy as sp
+import numpy as np
+from random import sample
+
+# Load image as matrix
+picFile = "C:/Users/blah/foo/bar.jpg"
+picMatrix = sp.misc.imread(picFile)
+
+# Change picture matrix from 3D to 2D
+# (pixels are rows, RGB values are columns)
+matrix2D = np.reshape(picMatrix, (-1, picMatrix.shape[-1]))
+
+# Choose K (num colors), max centroid iterations, and number of runs
+k = 3
+maxIter = 20
+numRuns = 50
+
+# Randomly initialize k centroids function
+def randomCen(matrix2D):
+    randIndicies = sample(range(0, matrix2D.shape[0]), k)
+    centroids = np.zeros((k, matrix2D.shape[1]))
+    for i in range(0, k):
+        centroids[i,:] = matrix2D[randIndicies[i],:]
+    return centroids
+
+# Point/cluster assignment function
 '''
-Load image
-
-Convert image to RGB matrix (3D or 4D)
-
-Choose K (num colors), max centroid iterations, and number of runs
-    k = 3
-    maxIter = 20
-    numRuns = 50
-
-Randomly initialize k centroids function
-    def randomCen(pictureArray)
-        return centroids = []
-
-Point/cluster assignment function
     def closestCen(pictureArray, centroids):
         return assignedPoints
+'''
         
-Move centroids function
+# Move centroids function
+'''
     def moveCen(assignedPoints, centroids):
         return centroids
+'''
 
-Function to run K-Means
+# Function to run K-Means
+'''
     def runKMeans(pictureArray, centroids, maxIter)
         return centroids, cost
+'''
 
-Run K-Means numRuns times
+# Run K-Means numRuns times
+'''
     for i in range(numRuns):
         centroids = randomCen(pictureArray)
         centroids, cost = runKMeans(pictureArray, centroids, maxIter)
@@ -34,6 +52,6 @@ Run K-Means numRuns times
         if cost < smallcost:
             bestCen = centroids
             bestPoints = closestCen(pictureArray, bestCen)
-
-Convert array back into image, then output
 '''
+
+# Convert array back into image, then output
