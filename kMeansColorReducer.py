d21b66f4fb6f3c9ff8f34b2cd9fe4cc0827ea1d6
Stehvin <stehvin.olson@gmail.com>
2017-07-22 11:03:31 -0500

    Add documentation and complete unit testing for imageToMatrix and randomCen functions

--- kMeansColorReducer.py
+++ kMeansColorReducer.py
@@ -1,59 +1,77 @@
 # -*- coding: utf-8 -*-
-
+import pdb
 import scipy as sp
 import numpy as np
 from random import sample
 
-# Load image as matrix
-picFile = "C:/Users/blah/foo/bar.jpg"
-picMatrix = sp.misc.imread(picFile)
+pdb.set_trace()
+# set file path for image
+picFile = r"C:\Users\Stehvin\Pictures\Chicago.jpg"
 
-# Change picture matrix from 3D to 2D
-# (pixels are rows, RGB values are columns)
-matrix2D = np.reshape(picMatrix, (-1, picMatrix.shape[-1]))
-
-# Choose K (num colors), max centroid iterations, and number of runs
+# choose K (num colors), max centroid iterations, and number of runs
 k = 3
 maxIter = 20
 numRuns = 50
 
-# Randomly initialize k centroids function
-def randomCen(matrix2D):
+def imageToMatrix(imageFilePath):
+    """Convert an image into an n x 3 matrix, where n is the total
+    number of pixels in the image, and the 3 columns represent each
+    pixel's RGB values.
+    """
+    # load image as matrix
+    picMatrix = sp.misc.imread(picFile)
+
+    # change picture matrix from 3D to 2D
+    # (pixels are rows, RGB values are columns)
+    matrix2D = np.reshape(picMatrix, (-1, picMatrix.shape[-1]))
+    return matrix2D
+
+def randomCen(matrix2D, k):
+    """Randomly initialize k centroids.
+    Given a 2D picture matrix, this function randomly picks k number 
+    of rows to assign to the centroids matrix.
+    
+    Example
+    ------
+    matrix2D -> 500 x 3
+    k = 4
+    centroids -> 4 x 3
+    """
     randIndicies = sample(range(0, matrix2D.shape[0]), k)
     centroids = np.zeros((k, matrix2D.shape[1]))
     for i in range(0, k):
         centroids[i,:] = matrix2D[randIndicies[i],:]
     return centroids
 
-# Point/cluster assignment function
+# point/cluster assignment function
 '''
-    def closestCen(pictureArray, centroids):
-        return assignedPoints
+def closestCen(pictureArray, centroids):
+    return assignedPoints
 '''
         
-# Move centroids function
+# move centroids function
 '''
-    def moveCen(assignedPoints, centroids):
-        return centroids
-'''
-
-# Function to run K-Means
-'''
-    def runKMeans(pictureArray, centroids, maxIter)
-        return centroids, cost
+def moveCen(assignedPoints, centroids):
+    return centroids
 '''
 
-# Run K-Means numRuns times
+# function to run K-Means
 '''
-    for i in range(numRuns):
-        centroids = randomCen(pictureArray)
-        centroids, cost = runKMeans(pictureArray, centroids, maxIter)
-        
-        if cost < smallcost:
-            bestCen = centroids
-            bestPoints = closestCen(pictureArray, bestCen)
+def runKMeans(pictureArray, centroids, maxIter)
+    return centroids, cost
+'''
+
+# run K-Means numRuns times
+'''
+for i in range(numRuns):
+    centroids = randomCen(pictureArray)
+    centroids, cost = runKMeans(pictureArray, centroids, maxIter)
+    
+    if cost < smallcost:
+        bestCen = centroids
+        bestPoints = closestCen(pictureArray, bestCen)
 
 '''
 
-# Convert array back into image, then output
+# convert array back into image, then output
 
