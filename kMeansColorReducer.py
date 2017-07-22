0a8c3467c0ac896e943e908e9842ebf5223fe466
Stehvin <stehvin.olson@gmail.com>
2017-07-22 09:36:53 -0500

    Add kMeansColorReducer.py file

---
+++ kMeansColorReducer.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+import pdb
+
+
+aaa = "heyor"
+pdb.set_trace()
+bbb = "saywhat"
+
+def combine(s1,s2):      # define subroutine combine, which...
+    s3 = s1 + s2 + s1    # sandwiches s2 between copies of s1, ...
+    s3 = '"' + s3 +'"'   # encloses it in double quotes,...
+    return s3
+final = combine(aaa,bbb)
+print(aaa + bbb)
+
+
+'''
+Load image
+
+Convert image to RGB matrix (3D or 4D)
+
+Choose K (num colors), max centroid iterations, and number of runs
+    k = 3
+    maxIter = 20
+    numRuns = 50
+
+Randomly initialize k centroids function
+    def randomCen(pictureArray)
+        return centroids = []
+
+Point/cluster assignment function
+    def closestCen(pictureArray, centroids):
+        return assignedPoints
+        
+Move centroids function
+    def moveCen(assignedPoints, centroids):
+        return centroids
+
+Function to run K-Means
+    def runKMeans(pictureArray, centroids, maxIter)
+        return centroids, cost
+
+Run K-Means numRuns times
+    for i in range(numRuns):
+        centroids = randomCen(pictureArray)
+        centroids, cost = runKMeans(pictureArray, centroids, maxIter)
+        
+        if cost < smallcost:
+            bestCen = centroids
+            bestPoints = closestCen(pictureArray, bestCen)
+
+Convert array back into image, then output
+'''
