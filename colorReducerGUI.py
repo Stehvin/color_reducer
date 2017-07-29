e2ce0ec7088a7c9800c242afb22d0d94d930e8b2
Stehvin <stehvin.olson@gmail.com>
2017-07-29 10:58:34 -0500

    Assign file path to a global variable

--- colorReducerGUI.py
+++ colorReducerGUI.py
@@ -2,17 +2,16 @@
 
 from tkinter import *
 
+inputFile = ""
 mainWin = Tk()
 
 def browseButton():
     """Allows the user to browse their files and select a .jpg or
     .png picture.
     """
-    filename = filedialog.askopenfilename(filetypes=[("Picture Files",
-                                                      "*.jpg;*.png")])
-    print(filename)
-    return filename
+    global inputFile 
+    inputFile = filedialog.askopenfilename(filetypes=[("Picture Files", 
+                                                       "*.jpg;*.png")])
 
 button2 = Button(text="Browse", command=browseButton).grid(row=0)
-
 mainWin.mainloop()
