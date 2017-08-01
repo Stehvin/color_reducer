7125052206bab5f384a81ba4ec50badeacca2951
Stehvin <stehvin.olson@gmail.com>
2017-07-31 20:43:06 -0500

    Add image output capabilities and begin formating screen dimensions

--- colorReducerGUI.py
+++ colorReducerGUI.py
@@ -1,9 +1,15 @@
 # Author: Stehvin Olson
 
 from tkinter import *
+from PIL import ImageTk, Image
+from win32api import GetSystemMetrics
+
+print("Width: ", GetSystemMetrics(0))
 
 inputFile = ""
 mainWin = Tk()
+mainWin.title("HEY")
+mainWin.geometry("{}x{}".format(GetSystemMetrics(0), GetSystemMetrics(1)))
 
 def browseButton():
     """Allows the user to browse their files and select a .jpg or
@@ -12,6 +18,14 @@ def browseButton():
     global inputFile 
     inputFile = filedialog.askopenfilename(filetypes=[("Picture Files", 
                                                        "*.jpg;*.png")])
+    img = ImageTk.PhotoImage(Image.open(inputFile))
+    pic1 = Label(mainWin, image=img)
+    pic1.image = img
+    pic1.pack(side='left', fill='both', expand=True)
 
-button2 = Button(text="Browse", command=browseButton).grid(row=0)
+button2 = Button(text="Browse", command=browseButton).pack(fill='both',
+                                                            expand=True)
+
+
+    
 mainWin.mainloop()
