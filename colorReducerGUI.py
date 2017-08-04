484914a9094c70419a43df2237371fb4b169c2b3
Stehvin <stehvin.olson@gmail.com>
2017-08-03 20:07:01 -0500

    Clean up function locations and add main function

--- colorReducerGUI.py
+++ colorReducerGUI.py
@@ -3,9 +3,42 @@
 from tkinter import *
 from PIL import ImageTk, Image
 
-mainWin = Tk()
-mainWin.title("Color-Limited Sketch")
-mainWin.geometry("900x550")
+def main():
+    # set up main GUI window
+    mainWin = Tk()
+    mainWin.title("Color-Limited Sketch")
+    mainWin.geometry("900x550")
+    
+    # set up input file location text entry and "Browse" button
+    inFile = Entry(mainWin)
+    inFile.config(width=50)
+    Label(mainWin, text="Input File Location:").grid(row=0, column=0, sticky=E)
+    inFile.grid(row=0, column=1, sticky=W)
+    button1 = Button(text="Browse", command=browseButton).grid(row=1, column=1)
+    
+    # set up output file location/name text entry and "Save As" button
+    outFile = Entry(mainWin)
+    outFile.config(width=50)
+    Label(mainWin, text="Output File Location/Name:").grid(row=4, column=0,
+                                                           sticky=E)
+    outFile.grid(row=4, column=1, sticky=W)
+    button2 = Button(text="Save As", 
+                     command=saveAsButton).grid(row=5, column=1)
+    
+    # set up number of colors text entry
+    kEntry = Entry(mainWin)
+    kEntry.config(width=5)
+    Label(mainWin, text="Number of Colors:").grid(row=8, column=0, sticky=E)
+    kEntry.grid(row=8, column=1, sticky=W)
+    
+    # set up k-means execute button
+    button3 = Button(text="Get Color-Limited Sketch",
+                     command=lambda: executeButton(inFile.get(), 
+                                                   outFile.get(), 
+                                                   kEntry.get())). \
+                     grid(row=11, column=1)
+        
+    mainWin.mainloop()
 
 def browseButton():
     """Allows user to browse their files and select a .jpg or
@@ -42,34 +75,6 @@ def executeButton(inputFile, outputFile, k):
     print(outputFile)
     print(k)
     return None
-    
-# set up input file location text entry and "Browse" button
-inFile = Entry(mainWin)
-inFile.config(width=50)
-Label(mainWin, text="Input File Location:").grid(row=0, column=0, sticky=E)
-inFile.grid(row=0, column=1, sticky=W)
-button1 = Button(text="Browse", command=browseButton).grid(row=1, column=1)
 
-# set up output file location/name text entry and "Save As" button
-outFile = Entry(mainWin)
-outFile.config(width=50)
-Label(mainWin, text="Output File Location/Name:").grid(row=4, column=0,
-                                                       sticky=E)
-outFile.grid(row=4, column=1, sticky=W)
-button2 = Button(text="Save As", 
-                 command=saveAsButton).grid(row=5, column=1)
-
-# set up number of colors text entry
-kEntry = Entry(mainWin)
-kEntry.config(width=5)
-Label(mainWin, text="Number of Colors:").grid(row=8, column=0, sticky=E)
-kEntry.grid(row=8, column=1, sticky=W)
-
-# set up k-means execute button
-button3 = Button(text="Get Color-Limited Sketch",
-                 command=lambda: executeButton(inFile.get(), 
-                                               outFile.get(), 
-                                               kEntry.get())). \
-                 grid(row=11, column=1)
-    
-mainWin.mainloop()
+if __name__ == "__main__":
+    main()
